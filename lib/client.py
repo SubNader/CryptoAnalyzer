6b08fb1b971158c059a8404bcd4ab5e1efa8ffe7
SubNader <naderalsharkawy@gmail.com>
2019-07-13 01:30:08 +0200

    Modularize client module save method

--- lib/client.py
+++ lib/client.py
@@ -3,6 +3,20 @@ from pathlib import Path
 from urllib.parse import urlencode
 from datetime import datetime as dt
 
+def raw_save_attributes(configurations, verify_directory=False):
+
+    timestamp = dt.now().strftime("%Y%m%d")
+    currency = configurations['api']['currency']
+    market = configurations['api']['market']
+    save_format = configurations['raw_data']['format']
+    save_directory = configurations['raw_data']['directory']
+    save_path = f"{save_directory}/{currency}_{market}_{timestamp}.{save_format}"
+
+    # Ensure save directory is present, if required
+    if verify_directory:
+        Path(save_directory).mkdir(parents=True, exist_ok=True)
+
+    return save_path, save_format
 
 def fetch_data(configurations, save=False):
     
@@ -27,16 +41,10 @@ def fetch_data(configurations, save=False):
     
     # Save data if required
     if save:
-        
-        # Create save path
-        save_format = configurations['storage']['format']
-        save_directory = configurations['storage']['directory']
-        timestamp = dt.now().strftime("%Y%m%d%H%M")
-        save_path = f"{save_directory}/{params.get('symbol')}_{params.get('market')}_{timestamp}.{save_format}"
 
-        # Ensure save directory is present
-        Path(save_directory).mkdir(exist_ok=True)
-        
+        # Get output attributes
+        save_path, save_format = raw_save_attributes(configurations, verify_directory=True)
+
         # Save data to disk based on configured format
         if save_format == 'json':
             data.to_json(save_path, index=None)
