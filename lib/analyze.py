ce79418dca9865da02c5537257c79e230081de88
SubNader <naderalsharkawy@gmail.com>
2019-07-13 02:20:29 +0200

    Implement maximum_relative_spans

--- lib/analyze.py
+++ lib/analyze.py
@@ -1,5 +1,7 @@
 import pandas as pd
+from . import logger
 from pathlib import Path
+from copy import deepcopy
 from datetime import datetime as dt
 
 
@@ -19,7 +21,9 @@ def analysis_save_attributes(configurations, mode, verify_directory=False):
     return save_path, save_format
 
 
-def weekly_mean_prices(data, configurations, save=False):
+def weekly_mean_prices(fetched_data, configurations, save=False):
+    
+    data = deepcopy(fetched_data)
     
     # Make the data timestamp-indexed
     data.reset_index(inplace=True)
@@ -37,6 +41,9 @@ def weekly_mean_prices(data, configurations, save=False):
     data.reset_index(drop=False, inplace=True)
     weekly_means = data[data.weekly_mean.notnull()][['week_start','weekly_mean']]
     
+    # Free memory
+    del data
+    
     if save:
         
         # Get output attributes
@@ -50,3 +57,39 @@ def weekly_mean_prices(data, configurations, save=False):
             
             
     return weekly_means
+
+
+def maximum_relative_spans(fetched_data, configurations, n_weeks=1):
+
+    data = deepcopy(fetched_data)
+
+    # Make the data timestamp-indexed
+    data.reset_index(inplace=True)
+    data['timestamp'] = pd.to_datetime(data['timestamp'], errors='coerce')
+    data.set_index('timestamp', inplace=True)
+    
+    # Resample data to weekly
+    data.resample('W-MON')
+    
+    # Get market symbol - needed for column name
+    market = configurations['api']['market']
+
+    # Compute min and max closing price per week
+    data['min_closing_price'] = data[f'close ({market})'].min()
+    data['max_closing_price'] = data[f'close ({market})'].max()
+    
+    # Compute weekly relative spans
+    data['relative_span'] = (data['max_closing_price'] - data['min_closing_price']) / data['max_closing_price']
+
+    # Clean up final dataframe
+    data.rename_axis('week_start', inplace=True)
+    data.reset_index(drop=False, inplace=True)
+    
+    # Get N weeks of maximum relative spans 
+    maximum_weekly_relative_spans = data.sort_values('relative_span', ascending=False)
+    max_n_relative_spans = maximum_weekly_relative_spans[['week_start', 'relative_span']].head(n_weeks)
+    
+    # Report value
+    logger.dataframe(max_n_relative_spans)
+    
+    
